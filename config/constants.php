01e1cafffab621a1934188f559c4feb1246a0c94
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-13 22:02:30 +0800

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,16 +1,14 @@
 <?php 
 
-    //session needed to be changed if deploying online for this is from local database
-    session_start();
-    
-        //create cnstnts to store non repeating values
-        define('SITEURL', 'http://localhost/zaxxun-ordering/');
-        define('LOCALHOST','localhost');
-        define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '');
-        define('DB_NAME', 'zaxxun_ordering');
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-        $conn = mysqli_connect(LOCALHOST, DB_USERNAME,DB_PASSWORD) or die(mysqli_error());
-        $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
